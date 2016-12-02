b1a1dbf8d0b831b0c415deb9c3b4a053163836a2
Abdullah <abdullahcaliskan511@gmail.com>
2016-12-02 15:13:47 +0300

    Tipler hakkında not ekleme

---
+++ annotations.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+Dipnotlar :
+
+Dipnotlar, bir tip hakkında ek meta bilgiler tutmamıza yardımcı olur
+caf::meta::annotation , caf::meta::is_annotation<T>::value ile sorgulanabilir. Tip olsun olmasın.
+
+type_name(n) : Okunabilir olarak çıktı ver, tip adı 'n' olanları göster.
+hex_formatted() : gelen dataları hex formatında yaz
+omittable() : gelen dataları atla.
+omittable_if_empty() : gelen dataları atla, eğer boşsa
+omittable_if_none() : none'a eşit olan dataları atla
+save_callback(f) : serileştirirken, f'i çağır
+load_callback(f) : deserileştirdikten sonra f'i çağır
+
+
+Geriye doğru ve 3.parti uyumluluk :
+
+
+*/
+
+
+int main()
+{
+
+}
